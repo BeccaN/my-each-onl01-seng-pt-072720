8bbdf85045b73f3fc566abebc7e318ce3f786cdc
Rebecca Congi <rncongi@gmail.com>
2020-08-06 19:51:39 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,13 @@
 def my_each(array)
+<<<<<<< HEAD
   i = 0 
   while i < array.length
     yield array[i]
     i += 1 
   end
   array
+=======
+  while 
+  
+>>>>>>> 73d3ec98aaa24bfa8f1a3c8e8956ff4b6e775edc
 end
